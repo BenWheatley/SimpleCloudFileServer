0b27dd71da6021c745e8990432402db90a6a0546
Ben Wheatley <ben@kitsunesoftware.com>
2017-10-03 13:08:26 +0200

    injection test; comment that further tests should be via metasploit

--- TestSimpleCloudFileServer.py
+++ TestSimpleCloudFileServer.py
@@ -15,3 +15,6 @@ upload_file: <how?> -> status==200, result==<json with number>
 download_file: url=="http://localhost:8000/<result of upload_file>" -> status==200, result==<identical image data to that which was uploaded>
 
 download_nonexistent_file: url=="http://localhost:8000/<value not in load_index>" -> status==404
+
+injection_attack_python: url=="http://localhost:8000/print(\"hello\")" -> status==404
+# Anything more than this level of injection-proof testing should be done with Metasploit, which will know more server failure modes than I've heard of
