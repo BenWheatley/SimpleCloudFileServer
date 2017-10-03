6aaf6ae4d11c820c5e29299d8d92289d0d8cd76a
Ben Wheatley <ben@kitsunesoftware.com>
2017-10-03 13:06:59 +0200

    some ideas for tests

--- TestSimpleCloudFileServer.py
+++ TestSimpleCloudFileServer.py
@@ -5,3 +5,11 @@ Copyright 2017 Ben Wheatley
 
 This is what needs to be tested, but I don't know how to write and run Python server unit tests yet.
 """
+
+# name: url -> status, result (optional)
+
+load_index: url=="http://localhost:8000/index.json" -> status==200
+
+upload_file: <how?> -> status==200, result==<json with number>
+
+download_file: url=="http://localhost:8000/<result of upload_file>" -> status==200, result==<identical image data to that which was uploaded>
