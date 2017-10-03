485f999e6b7a9a3e641eab7bd18b3c96b3bbf8bc
Ben Wheatley <ben@kitsunesoftware.com>
2017-10-03 13:07:36 +0200

    404 test

--- TestSimpleCloudFileServer.py
+++ TestSimpleCloudFileServer.py
@@ -13,3 +13,5 @@ load_index: url=="http://localhost:8000/index.json" -> status==200
 upload_file: <how?> -> status==200, result==<json with number>
 
 download_file: url=="http://localhost:8000/<result of upload_file>" -> status==200, result==<identical image data to that which was uploaded>
+
+download_nonexistent_file: url=="http://localhost:8000/<value not in load_index>" -> status==404
