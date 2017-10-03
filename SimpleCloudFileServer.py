b046019ce106f6f0a7ae70280edb9ddf454992f8
Ben Wheatley <ben@kitsunesoftware.com>
2017-10-03 12:46:25 +0200

    better logging, refactor for reusability

--- SimpleCloudFileServer.py
+++ SimpleCloudFileServer.py
@@ -8,18 +8,26 @@ HOST_NAME = "localhost"
 PORT_NUMBER = 8000 # Magic number. Can't bind under 1024 on normal user accounts; port 80 is the normal HTTP port
 
 class SimpleCloudFileServer(BaseHTTPServer.BaseHTTPRequestHandler):
-	def do_GET(self):
-		self.send_response(200)
-		self.send_header("Content-type", "text/plain")
+	def sendHeader(self, response=200, contentType="application/json"):
+		self.send_response(response)
+		self.send_header("Content-type", contentType)
 		self.end_headers()
+	
+	def do_GET(self):
+		self.sendHeader()
 		self.wfile.write("hi")
 
+def printServerMessage(customMessage):
+	print customMessage, "(Time: %s, Host: %s, port: %s)" % (time.asctime(), HOST_NAME, PORT_NUMBER)
+	
 if __name__ == '__main__':
 	server_class = BaseHTTPServer.HTTPServer
 	httpd = server_class((HOST_NAME, PORT_NUMBER), SimpleCloudFileServer)
 	
+	printServerMessage("Server startup")
 	try:
 		httpd.serve_forever()
 	except KeyboardInterrupt:
 		pass
 	httpd.server_close()
+	printServerMessage("Server stop")
