8fbebab0cf664c885ef3d9ad1ab110eebbabe3b9
Ben Wheatley <ben@kitsunesoftware.com>
2017-10-03 12:44:35 +0200

    minimal server

--- SimpleCloudFileServer.py
+++ SimpleCloudFileServer.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python
+
+import time
+import BaseHTTPServer
+import json
+
+HOST_NAME = "localhost"
+PORT_NUMBER = 8000 # Magic number. Can't bind under 1024 on normal user accounts; port 80 is the normal HTTP port
+
+class SimpleCloudFileServer(BaseHTTPServer.BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text/plain")
+		self.end_headers()
+		self.wfile.write("hi")
+
+if __name__ == '__main__':
+	server_class = BaseHTTPServer.HTTPServer
+	httpd = server_class((HOST_NAME, PORT_NUMBER), SimpleCloudFileServer)
+	
+	try:
+		httpd.serve_forever()
+	except KeyboardInterrupt:
+		pass
+	httpd.server_close()
